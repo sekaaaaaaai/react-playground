5f5854ab2163d8edfd29b156d07ef3d4eecb91e1
Adolman <iruru147@icloud.com>
2020-04-18 08:35:39 +0900

    implement states

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { useState } from 'react';
 
 const App = props => {
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
+  const [state, setState] = useState(props)
+  const { price, name } = state
+  // const [name, setName] = useState(props.name)
+  // const [price, setPrice] = useState(props.price)
   
   const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
+    // setPrice(props.price)
+    // setName(props.name)
+    setState(props)
   }
 
   return (
     <React.Fragment>
-      <p>現在の{name}は、{price}です</p>
-      <button onClick={ () => setPrice(price + 100) }>+100</button>
-      <button onClick={ () => setPrice(price - 100) }>-100</button>
+      <p>現在の{name}は、{price}円です</p>
+      <button onClick={ () => setState({...state, price: price + 100}) }>+100</button>
+      <button onClick={ () => setState({...state, price: price - 100}) }>-100</button>
       <button onClick={ reset }>Reset</button>
-      <input value={name} onChange={ e => setName(e.target.value) }/>
+      <input value={name} onChange={ e => setState({ ...state, name: e.target.value }) }/>
     </React.Fragment>
   );
 }
 
 App.defaultProps = {
-  name: 'sasaasas',
+  name: 'sakai',
   price: 1000
 }
 
